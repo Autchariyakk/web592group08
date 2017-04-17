fb068601d80de52750831b6fc81f167238b24e6c
Kiklik <autchariya.c@kkumail.com>
2017-04-17 18:15:56 +0700

    fileProject

---
+++ kiklikchaken/main.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+ <head>
+ <meta charset="utf-8">
+ <title>WEBWEB</title>
+ </head>
+ <link rel="stylesheet" href="kk.css">
+ <body><center>
+ 
+<h1 style="color:black;background-color:white;"" align="center" id="css">WEB Home Page</h1>
+ <marquee behavior="alternate" direction="up" width="100%"> <marquee direction="right" behavior="alternate"><h2 style="color:red"><b>Wellcome</b></h2></marquee> </marquee>
+ <center><img WIDTH=1280  HEIGHT=720   src="https://i.ytimg.com/vi/TSXEq1Vd_AA/maxresdefault.jpg"></center>
+<BODY background="https://i.ytimg.com/vi/tQPSiIHI_go/hqdefault.jpg"></BODY>
+<h2 style="color:#8888ff;background-color:pink" align="center">หนังสือการ์ตูนใหม่</h2>
+<div style="color:red;background-color:yellow" align="center" id="css">
+สมาชิก
+583021156-6 นางสาว อัจฉริยา ชาเคน
+583021154-0 นาย อนุสิษฐ โพธิ์เย็น
+583021151-6 นาย สรวิชญ์ ไชยเดช
+583021124-9 นาย กัณชาติ อภิสิทธิสานนท์
+สาขาเทคโนโลยีสารสนเทศและการสื่อสาร
+คณะวิทยาศาสตร์  KKU
+</div>
+<ul id='menubox'>
+<h1 style="color:gold;background-color:white" align="center" id="css">โหมดต่อสู้</h1>
+<h2><a href=home.html>ดูเพิ่ม</a><br></h2>
+
+<h1 style="color:gold;background-color:white" align="center" id="css">โหมดแฟนตาซี</h1>
+<h2><a href=work3.html>ดูเพิ่ม</a><br></h2>
+<h1 style="color:gold;background-color:white" align="center" id="css">โหมดโรแมนติก</h1>
+<h2><a href=work4.php>ดูเพิ่ม</a><br></h2>
+<h1 style="color:gold;background-color:white" align="center" id="css">โหมดความรู้</h1>
+<h2><a href=work5.php>ดูเพิ่ม</a><br></h2>
+
+</ul>
+</style> 
+ </body>
+</html>
