37a1de345647653c52aaa80f11417dae0b18c9f8
Kiklik <autchariya.c@kkumail.com>
2017-05-04 07:06:23 +0700

    Add files via upload

---
+++ body_about.php
@@ -0,0 +1,135 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Piccolo Theme</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- CSS
+================================================== -->
+<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/bootstrap-responsive.css">
+<link rel="stylesheet" href="css/jquery.lightbox-0.5.css">
+<link rel="stylesheet" href="css/custom-styles.css">
+
+<!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <link rel="stylesheet" href="css/style-ie.css"/>
+<![endif]--> 
+
+<!-- Favicons
+================================================== -->
+<link rel="shortcut icon" href="favicon.ico">
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+<!-- JS
+================================================== -->
+<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/jquery.custom.js"></script>
+
+
+</head>
+
+<body>
+	<div class="color-bar-1"></div>
+    <div class="color-bar-2 color-bg"></div>
+    
+    <div class="container main-container">
+    
+      <div class="row header"><!-- Begin Header -->
+      
+        <!-- Logo
+        ================================================== -->
+        <div class="span3 logo">
+        	<a href="main.php"><img src="img/pp.png" alt="" /></a>
+            <h3>กลุ่มที่ 8 Web592group08</h3>
+        </div>
+        
+        
+     
+    <!-- Blog Content
+    ================================================== --> 
+    <div class="row">
+
+        <!-- Blog Posts
+        ================================================== --> 
+        <div class="span8 blog">
+
+            <!-- Blog Post 1 -->
+            <article>
+                <div class="row">
+                    <div class="span2 blog-style-2">
+                        <h4 class="title-bg">4/5/2017</h4>
+                        <ul class="post-data">
+                            <li><i class="icon-user"></i> <a href="#">Admin</a></li>
+                            
+                        </ul>
+                        
+                    </div>
+                    <div class="span6">       
+                        <a href="#"><img src="img/kik.jpg" alt="Post Thumb"></a>
+                        <h3 class="title-bg"><a href="blog-single.php">Autchariya  Chakan</a></h3>
+                    </div>    
+                </div>
+            </article>
+
+            <!-- Blog Post 2 -->
+             <article>
+                <div class="row">
+                    <div class="span2 blog-style-2">
+                        <h4 class="title-bg">4/5/2017</h4>
+                        <ul class="post-data">
+                            <li><i class="icon-user"></i> <a href="#">Admin</a></li>
+                           
+                        </ul>
+                       
+                    </div>
+                    <div class="span6">       
+                        <a href="#"><img src="img/kan.jpg" alt="Post Thumb"></a>
+                        <h3 class="title-bg"><a href="blog-single.php">Kannachart Apisittisanon</a></h3>
+                        
+                    </div>    
+                </div>
+            </article>
+
+             <!-- Blog Post 3 -->
+             <article>
+                <div class="row">
+                    <div class="span2 blog-style-2">
+                        <h4 class="title-bg">4/5/2017</h4>
+                        <ul class="post-data">
+                            <li><i class="icon-user"></i> <a href="#">Admin</a></li>
+                        </ul>
+                  
+                    </div>
+                    <div class="span6">       
+                        <a href="blog-single.php"><img src="img/it.jpg" alt="Post Thumb"></a>
+                        <h3 class="title-bg"><a href="blog-single.php">Anusit Poyen</a></h3>
+                       
+                    </div>    
+                </div>
+            </article>
+			<!-- Blog Post 4 -->
+             <article>
+                <div class="row">
+                    <div class="span2 blog-style-2">
+                        <h4 class="title-bg">4/5/2017</h4>
+                        <ul class="post-data">
+                            <li><i class="icon-user"></i> <a href="#">Admin</a></li>
+                            
+                        </ul>
+                       
+                    </div>
+                    <div class="span6">       
+                        <a href="blog-single.php"><img src="img/four.jpg" alt="Post Thumb"></a>
+                        <h3 class="title-bg"><a href="blog-single.php">Sorawit Chaiyadet</a></h3>
+                        
+                    </div>    
+                </div>
+            </article>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<?php
+ $appid = "web592group08.appspot.com";
+ $page = $_GET['p'];
+ if($page=='') $page='main';
+ $title = $page;
+ function panel_include($title,$file,$ptype='default'){
+echo "<div class='panel panel-$ptype'>";
+echo "<div class='panel-heading'>$title</div>";
+echo "<div class='panel-body'>";
+if(file_exists($file)){
+ include($file);
+}else{
+ echo "ไม่พบไฟล์ $file ";
+}
+echo "</div>";
+echo "</div>";
+ }
+use google\appengine\api\cloud_storage\CloudStorageTools;
+function userpic($uid){
+ global $appid;
+ $userpic="gs://$appid/{$uid}.jpg";
+ if(!file_exists($userpic)){
+ return "img/profile.png";
+ }
+ return CloudStorageTools::getImageServingUrl($userpic,["size"=>200]);
+}
+  include_once("config.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>MOOPA BIPOLAR GIRL... SHOP</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/navbar-fixed-top.css" rel="stylesheet">
+
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- CSS
+================================================== -->
+<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/bootstrap-responsive.css">
+<link rel="stylesheet" href="css/prettyPhoto.css" />
+<link rel="stylesheet" href="css/flexslider.css" />
+<link rel="stylesheet" href="css/custom-styles.css">
+
+
+<!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <link rel="stylesheet" href="css/style-ie.css"/>
+<![endif]--> 
+
+<!-- Favicons
+================================================== -->
+<link rel="shortcut icon" href="favicon.ico">
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+<!-- JS
+================================================== -->
+<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/jquery.prettyPhoto.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/jquery.custom.js"></script>
+<script type="text/javascript">
+$(document).ready(function () {
+
+    $("#btn-blog-next").click(function () {
+      $('#blogCarousel').carousel('next')
+    });
+     $("#btn-blog-prev").click(function () {
+      $('#blogCarousel').carousel('prev')
+    });
+
+     $("#btn-client-next").click(function () {
+      $('#clientCarousel').carousel('next')
+    });
+     $("#btn-client-prev").click(function () {
+      $('#clientCarousel').carousel('prev')
+    });
+    
+});
+
+ $(window).load(function(){
+
+    $('.flexslider').flexslider({
+        animation: "slide",
+        slideshow: true,
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+    });  
+});
+
+</script>
+
+</head> 
+ </head>
+
+  <body>
+<BODY background="page-bg-1.jpg"></BODY>
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+		 
+          <a class="navbar-brand" href="index.php"><img src="img/pp.png" width="150" height="100"alt="" /></a>
+		 
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+		 <?php 
+		   include_once("menu.php");
+		 ?>         
+        </div>
+	  </div>	
+    </nav>
+
+    <div class="container">
+<?php
+   include_once("body.php");  
+?>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </body>
+</html>
+<!-- Footer Area
+        ================================================== -->
+
+	
+    
+    <!-- Scroll to Top -->  
+    <div id="toTop" class="hidden-phone hidden-tablet">Back to Top</div>
+    
+</body>
+</html>
